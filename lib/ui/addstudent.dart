da9486282cc0bd42605a5ded734363dc21ce6ade
eng.aladinhamod@gmail.com <eng.aladinhamod@gmail.com>
2019-09-07 21:09:21 +0100

    New Post Working

--- lib/ui/addstudent.dart
+++ lib/ui/addstudent.dart
@@ -4,9 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:studentapp/models/api.services.dart';
 import 'package:studentapp/models/student.dart';
 
-class AddStudent extends StatefulWidget { 
-final Student student;
-AddStudent(this.student);
+class AddStudent extends StatefulWidget {
+  final Student student;
+  AddStudent(this.student);
 
   @override
   State<StatefulWidget> createState() => _AddStudentState(student);
@@ -16,19 +16,19 @@ class _AddStudentState extends State<AddStudent> {
   Student student;
   _AddStudentState(this.student);
   var firstNameController = TextEditingController();
-  var lastNameController = TextEditingController(); 
-  final choices=const <String>['Save',  'Delete'];
-  final String menuSave='Save';
-  final String menuDelete='Delete';
+  var lastNameController = TextEditingController();
+  final options = const <String>['Save', 'Delete'];
+  static const menuSave = 'Save';
+  static const menuDelete = 'Delete';
   final _gendersDropDownList = ["Male", "Female", "NA"];
   String _genderDropDownList = "NA";
-  var textStyle=TextStyle();
+  var textStyle = TextStyle();
 
   @override
   Widget build(BuildContext context) {
-    firstNameController.text=student.firstName;
-    lastNameController.text=student.lastName;
-    textStyle=Theme.of(context).textTheme.title;
+    firstNameController.text = student.firstName;
+    lastNameController.text = student.lastName;
+    textStyle = Theme.of(context).textTheme.title;
     return Scaffold(
       appBar: _buildAppBar(context),
       body: _buildForm(),
@@ -40,12 +40,12 @@ class _AddStudentState extends State<AddStudent> {
       title: Text("Add Student"),
       actions: <Widget>[
         PopupMenuButton<String>(
-          onSelected: null,
-          itemBuilder: (BuildContext context){
-            return choices.map((String choice){
+          onSelected: selectOption,
+          itemBuilder: (BuildContext context) {
+            return options.map((String option) {
               return PopupMenuItem<String>(
-                value: choice,
-                child: Text(choice),
+                value: option,
+                child: Text(option),
               );
             }).toList();
           },
@@ -56,55 +56,53 @@ class _AddStudentState extends State<AddStudent> {
 
   Widget _buildForm() {
     return Padding(
-      padding: EdgeInsets.only(top: 35.0,left: 10.0,right: 10.0),
+      padding: EdgeInsets.only(top: 35.0, left: 10.0, right: 10.0),
       child: ListView(
         children: <Widget>[
           TextField(
             controller: firstNameController,
-            style:textStyle,
-            onChanged: null,
+            style: textStyle,
+            onChanged: (value)=> this.updateFirstName(),
             decoration: InputDecoration(
-              labelText: "First Name",
-              labelStyle: textStyle,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(5.0),                
-              )
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 15.0,bottom: 15.0),
-            child: TextField(
-              controller: lastNameController,
-              style: textStyle,
-              onChanged: null,
-              decoration: InputDecoration(
-                labelText: "Last Name",
+                labelText: "First Name",
                 labelStyle: textStyle,
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(5.0),
-                )
-              ),
+                )),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+            child: TextField(
+              controller: lastNameController,
+              style: textStyle,
+              onChanged:  (value)=> this.updateLastName(),
+              decoration: InputDecoration(
+                  labelText: "Last Name",
+                  labelStyle: textStyle,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  )),
             ),
           ),
-          ListTile(title: DropdownButton<String>(
-            items: _gendersDropDownList.map((String value){
+          ListTile(
+              title: DropdownButton<String>(
+            items: _gendersDropDownList.map((String value) {
               return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
+                value: value,
+                child: Text(value),
               );
             }).toList(),
             style: textStyle,
-            value:retrieveGender(student.gender),
-            onChanged: (value)=>setGender(value),             
-            ))          
+            value: retrieveGender(student.gender),
+            onChanged: (value) => updateGender(value),
+          ))
         ],
-      ), 
-    ) ;
+      ),
+    );
   }
 
-
- void setGender(String value) {  
-   switch (value) {
+  void updateGender(String value) {
+    switch (value) {
       case "Male":
         student.gender = 1;
         break;
@@ -112,18 +110,48 @@ class _AddStudentState extends State<AddStudent> {
         student.gender = 2;
         break;
       case "NA":
-         student.gender = 3;
+        student.gender = 3;
         break;
-    } 
+    }
     setState(() {
-          _genderDropDownList=value;
-        });
+      _genderDropDownList = value;
+    });
   }
 
-  String  retrieveGender(int value){
-    return _gendersDropDownList[value-1];
+void updateFirstName (){
+  student.firstName=firstNameController.text;
+}
+
+void updateLastName (){
+  student.lastName=lastNameController.text;
+}
+
+
+  String retrieveGender(int value) {
+    return _gendersDropDownList[value - 1];
   }
 
+  void selectOption(String value) async {
+    switch (value) {
+      case menuSave:
+        saveStudent();
+        break;
+      case menuDelete:
+        deleteStudent();
+        break;
+      default:
+    }
+  }
 
+  void saveStudent() {
+    print('save and back');     
+    var tes=APIServices.postStudent(student);
+    print('Post Data');
+    print(tes);
+    Navigator.pop(context, true);
+  }
 
+  void deleteStudent() {
+    print('we are working one delete student');
+  }
 }
